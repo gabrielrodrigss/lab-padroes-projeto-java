b8aaabdc0987f6ad12f45ac28fc13142eac1df51
Gabriel Oliveira Rodrigues <gabrielrrodrigues133@gmail.com>
2023-05-27 17:49:43 -0300

    Initial commit

---
+++ src/subsistema2/crm/CepApi.java
@@ -0,0 +1,23 @@
+package subsistema2.crm;
+
+import one.digitalinnovation.gof.singleton.SingletonEager;
+
+public class CepApi {
+    private static CepApi instancia = new CepApi();
+
+    private CepApi() {
+        super();
+    }
+
+    public static CepApi getInstancia() {
+        return instancia;
+    }
+
+    public String recuperarCidade(String cep) {
+        return "Araraquara";
+    }
+
+    public String recuperarEstado(String cep) {
+        return "SP";
+    }
+}
